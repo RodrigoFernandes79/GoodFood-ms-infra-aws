2c3b2d11c5bbbc9704fc32a772840e3372900c45
Rodrigo Fernandes <rodrigohf79@hotmail.com>
2024-12-05 20:57:16 -0300

    fix(cluster): add cluster attribute and getter in ClusterGoodFoodStack
    - Added 'Cluster cluster' attribute to 'ClusterGoodFoodStack' class
    - Implemented getter method 'getCluster()' to access the cluster instance

--- src/main/java/com/myorg/ClusterGoodFoodStack.java
+++ src/main/java/com/myorg/ClusterGoodFoodStack.java
@@ -8,15 +8,20 @@ import software.constructs.Construct;
 
 
 public class ClusterGoodFoodStack extends Stack {
+    private Cluster cluster;
+
     public ClusterGoodFoodStack(final Construct scope, final String id, final Vpc vpc) {
         this(scope, id, null, vpc);
     }
 
     public ClusterGoodFoodStack(final Construct scope, final String id, final StackProps props, final Vpc vpc) {
         super(scope, id, props);
-        Cluster cluster = Cluster.Builder.create(this, "GoodFoodCluster")
+        cluster = Cluster.Builder.create(this, "GoodFoodCluster")
                 .clusterName("GoodFood-Cluster")
                 .vpc(vpc).build();
     }
 
+    public Cluster getCluster() {
+        return cluster;
+    }
 }
